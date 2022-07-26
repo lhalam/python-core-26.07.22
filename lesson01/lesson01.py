f4f7486fb074fa0091513d8ac379da9d1236c2ea
Liubomyr Halamaha <galamagal86@gmail.com>
2022-07-26 20:38:38 +0300

    added lesson 01

---
+++ lesson01/lesson01.py
@@ -0,0 +1,27 @@
+print("a")
+
+
+def f():
+    print("f1")
+    print("f2")
+    print("f3")
+
+
+print("a2")
+print("a3")
+f()
+f()
+f()
+f()
+# a;
+a = 17
+a = 16
+testVariableTemp_10 = 10
+test_variable_temp_ten = 10
+_1_test_variable_temp_ten = 1
+a = 1
+print(a == test_variable_temp_ten)
+print(a == _1_test_variable_temp_ten)
+A = 1
+import sys
+print(sys.path)
