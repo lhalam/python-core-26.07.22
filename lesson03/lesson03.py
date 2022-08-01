acc49b726524ca76fcdfad402719c681dd090c66
Liubomyr Halamaha <galamagal86@gmail.com>
2022-08-01 19:56:04 +0300

    addet lesson03

---
+++ lesson03/lesson03.py
@@ -0,0 +1,90 @@
+# a = 10
+# print(type(a))
+# a1 = "10"
+# print(type(a1))
+# a1 = ["10"]
+# print(type(a1))
+
+a = 2**65+1-1+1
+print(id(a))
+a = 12
+print(id(a))
+a = 2**65+1-1+1
+print(id(a))
+a = 12
+print(id(a))
+print(2**7)
+
+
+# a = 1
+# print(id(a))
+# a += 1
+# print(id(a))
+# a = [1]
+# print(id(a))
+# a.append(1)
+# print(id(a))
+#
+# a = "hdsnvbjs"
+# print(a[5])
+# a = a[:5] + 'X' + a[6:]
+# print(a)
+# a = [1, 2, 3, "dsbh", 5.3, [1,2,3]]
+# print(a)
+# a[3] = "33"
+# a[5][0]='11'
+# a[5].append('11')
+# print(a)
+# a = (1,2,3)
+# print(a)
+# # a[1] = 1
+# print([i for i in dir(list) if not i[0]=="_"])
+# print([i for i in dir(tuple) if not i[0]=="_"])
+
+# print((-1)**(0.5))
+#
+# a = int()
+# a = int("10", 36)
+# print(a)
+
+a = 1
+
+print(type(a))
+s = type(a)
+print(s, s == "<class int'>")
+print(type(a) is int)
+print(type(a) is bool)
+name = "liubomyr"
+s = "my name %s %s" % (name, 16)
+print(s)
+s = "my name {} {}".format(name, name)
+
+def  f (name):
+    return name[::-1]
+print(f"my name {name} {16*36} {f(name)} {f}")
+a = "0123456789abcdef"
+print(a[0])
+print(a[10])
+# print(a[100])
+print(a[-1])
+print(a[-0])
+print(a[1:5])
+print(a[-1:-5])
+print(a[-5:-1])
+print(a[1:-1+6])
+print(a[1:-1+6])
+print(a[-1:0:-1])
+print(a[-1:0:-2])
+print(a[5:16:3])
+print(a[::3])
+print(a[:-1:3])
+print(a[::-1])
+
+import math
+
+
+
+
+
+
+
