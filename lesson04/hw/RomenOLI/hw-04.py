6b6a7df7deed86a206da893cdc045df8adbba3df
Roman Oleynik <romenoli@gmail.com>
2022-08-08 14:28:58 +0300

    second task

--- lesson04/hw/RomenOLI/hw-04.py
+++ lesson04/hw/RomenOLI/hw-04.py
@@ -1,12 +1,32 @@
 #Ввести значення(рік), вивести повідомлення "It's a leap year!" якщо рік високосний і "This is not a leap year!" якщо ні
-year = int(input("please, input year - "))
-if year % 4 == 0:
-    if year % 100 != 0:
-        print("It's a leap year!")
-    else:
-        if year % 400 == 0:
-            print("It's a leap year!")
+
+def is_lipyear(year):
+    text = ""
+    if year % 4 == 0:
+        if year % 100 != 0:
+            text = "It's a leap year!"
         else:
-            print("This is not a leap year!")
+            if year % 400 == 0:
+                text = "It's a leap year!"
+            else:
+                text = "This is not a leap year!"
+    else:
+        text = "This is not a leap year!"
+    return text
+
+# print(is_lipyear(int(input("please, input year - "))))
+
+#Ввести три значення (рік, місяць, день) у відповідні змінні. Визначити чи введені дані відповідають коректному запису дати
+year, month, day = int(input("input year  - ")), int(input("input month  - ")), int(input("input day  - "))
+
+if month in (1, 3, 5, 7, 8, 10, 12) and day == 31:
+    print("It is correct")
+elif month in (4, 6, 8, 11) and day == 30:
+    print("It is correct")
+elif month == 2:
+    if is_lipyear(year) == "It's a leap year!" and day == 29:
+        print("It is correct")
+    else:
+        print("It is not correct")
 else:
-    print("This is not a leap year!")
+    print("It is not correct")
