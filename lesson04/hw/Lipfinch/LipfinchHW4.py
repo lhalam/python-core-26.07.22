67c08f6cb2a29053d917c0df360ded4d080d0b01
Lipfinch <y.svyrydenko@gmail.com>
2022-08-08 12:14:07 +0300

    Lipfinch.py

---
+++ lesson04/hw/Lipfinch/LipfinchHW4.py
@@ -0,0 +1,12 @@
+# 1)""" Вести значення(рік), вивести повідомлення "It's a leap year! якщо рік високосний та "It's not a leap year!", якщо рік ні""""
+
+year = int(input("Введіть рік: "))
+
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} It's a leap year".format(year))
+
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} It's a leap year!".format(year))
+
+else:
+    print("{0} It's not a leap year".format(year))
