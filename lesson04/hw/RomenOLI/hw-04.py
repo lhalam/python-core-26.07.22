378cb8bd721bf32049f6dc3259190f918b34c3a8
Roman Oleynik <romenoli@gmail.com>
2022-08-08 16:42:27 +0300

    all task 04 hw

--- lesson04/hw/RomenOLI/hw-04.py
+++ lesson04/hw/RomenOLI/hw-04.py
@@ -1,4 +1,5 @@
 #Ввести значення(рік), вивести повідомлення "It's a leap year!" якщо рік високосний і "This is not a leap year!" якщо ні
+import math
 
 def is_lipyear(year):
     text = ""
@@ -14,35 +15,49 @@ def is_lipyear(year):
         text = "This is not a leap year!"
     return text
 
-# print(is_lipyear(int(input("please, input year - "))))
+print(is_lipyear(int(input("please, input year - "))))
 
 #Ввести три значення (рік, місяць, день) у відповідні змінні. Визначити чи введені дані відповідають коректному запису дати
-# year, month, day = int(input("input year  - ")), int(input("input month  - ")), int(input("input day  - "))
-#
-# if month in (1, 3, 5, 7, 8, 10, 12) and day == 31:
-#     print("It is correct")
-# elif month in (4, 6, 8, 11) and day == 30:
-#     print("It is correct")
-# elif month == 2:
-#     if is_lipyear(year) == "It's a leap year!" and day == 29:
-#         print("It is correct")
-#     else:
-#         print("It is not correct")
-# else:
-#     print("It is not correct")
+year, month, day = int(input("input year  - ")), int(input("input month  - ")), int(input("input day  - "))
+
+if month in (1, 3, 5, 7, 8, 10, 12) and day <= 31:
+    print("It is correct date")
+elif month in (4, 6, 8, 11) and day <= 30:
+    print("It is correct date")
+elif month == 2:
+    if is_lipyear(year) == "It's a leap year!" and day <= 29:
+        print("It is correct date")
+    elif is_lipyear(year) != "It's a leap year!" and day <= 28:
+        print("It is correct date")
+    else:
+        print("It is not correct date")
+else:
+    print("It is not correct date")
 
 # Для довільних дійсних чисел a, b, і c визначити, чи має рівняння ax2+bx+c=0 хоча б один дійсний корінь і якщо так, то видрукувати його.
+a, b, c = int(input("введи число a - ")), int(input("введи число b - ")), int(input("введи число c - ")),
+d = b**2 - 4*a*c
+
+if d > 0:
+    print(f"корінь: {(-b + math.sqrt(d))/(2*a)}")
+elif d == 0:
+    print(f"корінь: {-b / (2 * a)}")
+else:
+    print("немає дійсних коренів")
 
 # Задано три довільних числа. Визначити, чи можна побудувати трикутник з такими довжинами сторін; Якщо так, то видрукувати його периметр та площу.
-# import math
-# a, b, c = float(input("input a - ")), float(input("input b - ")), float(input("input c - ")),
-# if a > 0 and b > 0 and c > 0:
-#     print(f"периметр - {a+b+c}")
-#     p = (a+b+c)/2
-#     pl = math.sqrt(p*(p-a)*(p-b)*(p-c))
-#     print(f"площа - {pl}")
-# else:
-#     print("it is not a triangle")
+
+a, b, c = float(input("введи сторону трикутника a - ")), float(input("введи сторону трикутника b - ")), float(input("введи сторону трикутника c - ")),
+if a > 0 and b > 0 and c > 0:
+    if a+b >c or a+c > b or b+c > a:
+        print(f"периметр - {a+b+c}")
+        p = (a+b+c)/2
+        pl = math.sqrt(p*(p-a)*(p-b)*(p-c))
+        print(f"площа - {pl}")
+    else:
+        print("it is not a triangle")
+else:
+    print("it is not a triangle")
 
 # Нехай k- ціле від 1 до 365. Присвоїти цілій змінній n значення (понеділок, вівторок, …, суботу чи неділю) залежно від того , на який
 # день тижня припадає k-й день не високосного року, в якому 1 січня - понеділок
