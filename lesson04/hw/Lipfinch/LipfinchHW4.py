c9ada4deb55d905a756444f00a6c3da8bd254e41
Lipfinch <y.svyrydenko@gmail.com>
2022-08-08 14:14:44 +0300

    Lipfinch.py

--- lesson04/hw/Lipfinch/LipfinchHW4.py
+++ lesson04/hw/Lipfinch/LipfinchHW4.py
@@ -14,10 +14,30 @@ else:
 # 2) Ввести три значення (рік, місяць, день) у відповідні змінні. Визначити
 #   чи введені дані відповідають коректному запису дати.
 
-year = int(input())
-month = int(input())
-day = int(input())
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+day = int(input("Enter a day:" ))
 
+if year <= 2022:
+    print('')
+    if 1 <= month <= 12:
+        print('')
+        if 1 <=  day <= 31:
+            print('')
+            if month in [1, 3, 5, 7, 8, 10, 12] and day in range(1, 32):
+                print('')
+            elif month in [4, 6, 9, 11] and day in range(1, 31):
+                print('')
+            elif (year % 400 == 0 and month == 2 and day in range(1, 30)) or month == 2 and day in range(1, 29):
+                print("")
+            else:
+                print('Enter another day: ')
+
+    else:
+        print('Enter another month:' )
+
+else:
+    print("Enter another year:" )
 
 
 
