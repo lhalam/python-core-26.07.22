53ebb643ca26705dd9992e3298ef07e67a3f0caf
Lipfinch <y.svyrydenko@gmail.com>
2022-08-08 12:47:49 +0300

    Lipfinch.py

--- lesson04/hw/Lipfinch/LipfinchHW4.py
+++ lesson04/hw/Lipfinch/LipfinchHW4.py
@@ -1,6 +1,6 @@
-# 1)""" Вести значення(рік), вивести повідомлення "It's a leap year! якщо рік високосний та "It's not a leap year!", якщо рік ні""""
+# 1) Вести значення(рік), вивести повідомлення "It's a leap year!" якщо рік високосний та "It's not a leap year!", якщо рік ні
 
-year = int(input("Введіть рік: "))
+year = int(input("Enter a year: "))
 
 if (year % 400 == 0) and (year % 100 == 0):
     print("{0} It's a leap year".format(year))
@@ -10,3 +10,21 @@ elif (year % 4 ==0) and (year % 100 != 0):
 
 else:
     print("{0} It's not a leap year".format(year))
+
+# 2) Ввести три значення (рік, місяць, день) у відповідні змінні. Визначити
+#   чи введені дані відповідають коректному запису дати.
+
+year = int(input())
+month = int(input())
+day = int(input())
+
+
+
+
+
+
+
+
+
+
+
