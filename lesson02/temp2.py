06d7806a558a0b27a64fe2a78f2510b0602d9123
MyloslavaM <myloslava.martynova@gmail.com>
2022-08-08 20:07:52 +0300

    add HW

--- lesson02/temp2.py
+++ lesson02/temp2.py
@@ -1,1 +1,10 @@
 #test
+a = 77
+b = 77
+if b == 5:
+    print("a=5")
+elif b == 6:
+    print("a=6")
+elif b ==6:
+    print("a=7")
+
---
+++ lesson03/HW/__init__.py
@@ -0,0 +1,41 @@
+a = '''Beautiful is better  than ugly
+Explisit is better than implicit
+Simple is better than complex
+Complex is better than complicated
+Flat is better than nested
+Sparse is better then dense
+Readability counts
+Spesial cases aren`t spesial enough to break the rules
+Although practiality beats purity
+Errors should never pass silently
+Unless explicitly silenced
+In the face of ambiguity, refuse the temptation to guess
+There should be one-- and preferably only one --obvious way to do it
+Although that way may not be obvious at first unless you're Dutch'
+Now is better4 then never
+Although never is often better than *right* now
+If the implementation is hard to explain it's a bad idea
+If the implementation is easy to explain, it may be a good idea
+Namespaces are one honking great idea --let's do more of those!'''
+# print(a)
+print(a.count("better"))
+print(a.count("never"))
+print(a.count("is"))
+print(a.upper())
+
+i = 6587
+x = 6 * 5 * 8 * 7
+print (int(x))
+y = str(x)
+print(str(y[::-1]))
+
+r = [6, 5, 8, 7]
+r.sort()
+print(r)
+
+i = 3
+x = 5
+i, x = x, i
+print(i)
+
+
