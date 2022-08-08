a7ad8d3284caff311e66ae2aade70066b06c4af7
mrNERO <mrnero1986@gmail.com>
2022-08-08 18:38:06 +0300

    added rew hw 04

---
+++ lesson04/lesson04.py
@@ -0,0 +1,30 @@
+#a = 5
+#b = 4
+
+#print(a == b)
+#print(a != b)
+#print(not a == b)
+
+#a = "sdsaf"
+#b = "cvds"
+#print(a > B)
+
+# print(ord("a"))
+# print(chr(97))
+
+# for i in range(1000) :
+#     print(f"{i} - {chr(i)}")
+#
+# print(True > False)
+
+a = 10
+if a < 10:
+    print("a is > 10")
+
+
+if a < 10
+    if a < 5:
+        print("a <5")
+    else:
+        print("a [5, 9]")
+else
