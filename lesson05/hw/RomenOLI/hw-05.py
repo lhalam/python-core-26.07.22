15486d4325d1de8231238aba64e08f586d885a64
Roman Oleynik <romenoli@gmail.com>
2022-08-10 08:38:08 +0300

    fifth task

--- lesson05/hw/RomenOLI/hw-05.py
+++ lesson05/hw/RomenOLI/hw-05.py
@@ -31,19 +31,37 @@ import random
 
 # 4. Вивести на екран «прямокутник» розміру N на M, утворений з двох видів символів.
 # Контур прямокутника повинен складатися з одного символу, а "заливка" - з іншого.
-n, m = int(input("input N side - ")), int(input("input M side - "))
-border, zalivka = input("input sign for border - "), input("input sign for filling - ")
-for i in range(n):
-    if i == 0 or i == n-1:
-        print(border * m, end="\n")
-    else:
-        print(border + zalivka * (m-2) + border, end="\n")
+# n, m = int(input("input N side - ")), int(input("input M side - "))
+# border, zalivka = input("input sign for border - "), input("input sign for filling - ")
+# for i in range(n):
+#     if i == 0 or i == n-1:
+#         print(border * m, end="\n")
+#     else:
+#         print(border + zalivka * (m-2) + border, end="\n")
 
 # 5. Дано число P  і число H. Користувач вводить послідовність чисел.
 # Визначити: суму тих чисел, які менше P;
 # добуток чисел, які більші за H;
 # кількість чисел, що знаходяться  в діапазоні значень від P до H.
 # При введенні числа рівного P або H, припинити обчислення та вивести результат. (не використовувати білдін функції)
+p, h = int(input("input number P - ")), int(input("input number H - "))
+n = int(input("ВВеди число - "))
+sum, count, dob = 0, 0, 1
+while n != 0:
+    if n == p or n == h:
+        print(f"сума чисел, що менші Р = {sum}")
+        print(f"добуток чисел, що більше Н = {dob}")
+        print(f"кількість чисел, що в діапазоні між Р і Н = {count}")
+        break
+    elif n<p:
+        sum += n
+    elif n>h:
+        dob *= n
+
+    if p<=n<=h:
+        count +=1
+
+    n = int(input("ВВеди число - "))
 
 # 6. Для чисел, що вводяться користувачем, визначити відсоток додатних та від’ємних чисел. При введенні числа 0 закінчити роботу.
 # dod, vid = 0, 0
