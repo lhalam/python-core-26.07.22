c23e67aff38fb723006aec091a085c38eab16e2e
Roman Oleynik <romenoli@gmail.com>
2022-08-10 08:46:32 +0300

    all tasks

--- lesson05/hw/RomenOLI/hw-05.py
+++ lesson05/hw/RomenOLI/hw-05.py
@@ -5,39 +5,42 @@
 
 import random
 
-# n = random.randint(0, 100)
-# t = 1
-# while t <= 10:
-#     print(f"You have {11-t} shots" if (11-t)>1 else "You have last chanse")
-#     user_numb = int(input("please, input your number from 0 to 100 - "))
-#     if user_numb == n:
-#         print(f"Yes, you WIN!!!")
-#         break
-#     else:
-#         print("Nice try, but you didnt guess. Your number is bigger" if user_numb > n else "Nice try, but you didnt guess. Your number is smaller")
-#     t += 1
-#
-# print(f"Random number was - {n}" if t > 10 else "It was great")
+n = random.randint(0, 100)
+t = 1
+while t <= 10:
+    print(f"You have {11-t} shots" if (11-t)>1 else "You have last chanse")
+    user_numb = int(input("please, input your number from 0 to 100 - "))
+    if user_numb == n:
+        print(f"Yes, you WIN!!!")
+        break
+    else:
+        print("Nice try, but you didnt guess. Your number is bigger" if user_numb > n else "Nice try, but you didnt guess. Your number is smaller")
+    t += 1
+
+print(f"Random number was - {n}" if t > 10 else "It was great")
 
 # 2. Вводяться десять натуральних чисел більше 2. Порахувати, скільки серед них чисел, що кратні 5-ти. (не використовувати лісти)
-# count = 0
-# for i in range(10):
-#     if int(input("input number >2 - ")) % 5 == 0:
-#         count +=1
-# print(f"there are - {count} number(s) divided 5")
+count = 0
+for i in range(10):
+    if int(input("input number >2 - ")) % 5 == 0:
+        count +=1
+print(f"there are - {count} number(s) divided 5")
 
 # 3. Вивести на екран таблицю множення (від 1 до 9).
-
+for i in range(1,10):
+    print(f"--- на {i} ---")
+    for j in range(1,10):
+        print(f"{i} * {j} = {i*j}")
 
 # 4. Вивести на екран «прямокутник» розміру N на M, утворений з двох видів символів.
 # Контур прямокутника повинен складатися з одного символу, а "заливка" - з іншого.
-# n, m = int(input("input N side - ")), int(input("input M side - "))
-# border, zalivka = input("input sign for border - "), input("input sign for filling - ")
-# for i in range(n):
-#     if i == 0 or i == n-1:
-#         print(border * m, end="\n")
-#     else:
-#         print(border + zalivka * (m-2) + border, end="\n")
+n, m = int(input("input N side - ")), int(input("input M side - "))
+border, zalivka = input("input sign for border - "), input("input sign for filling - ")
+for i in range(n):
+    if i == 0 or i == n-1:
+        print(border * m, end="\n")
+    else:
+        print(border + zalivka * (m-2) + border, end="\n")
 
 # 5. Дано число P  і число H. Користувач вводить послідовність чисел.
 # Визначити: суму тих чисел, які менше P;
@@ -64,12 +67,12 @@ while n != 0:
     n = int(input("ВВеди число - "))
 
 # 6. Для чисел, що вводяться користувачем, визначити відсоток додатних та від’ємних чисел. При введенні числа 0 закінчити роботу.
-# dod, vid = 0, 0
-# n = int(input("ВВеди число - "))
-# while n != 0:
-#     if n>0:
-#         dod += 1
-#     else:
-#         vid += 1
-#     n = int(input("ВВеди число - "))
-# print(f"додаткових чисел = {100 * dod/(dod + vid)}% \nвід’ємних чисел = {100 * vid/(dod + vid)}%")
+dod, vid = 0, 0
+n = int(input("ВВеди число - "))
+while n != 0:
+    if n>0:
+        dod += 1
+    else:
+        vid += 1
+    n = int(input("ВВеди число - "))
+print(f"додаткових чисел = {100 * dod/(dod + vid)}% \nвід’ємних чисел = {100 * vid/(dod + vid)}%")
