6a712d6471584a8a3856978da9f2b4d8c90020e5
mrmey17 <paschokk95@gmail.com>
2022-08-10 17:21:57 +0300

    task #1

---
+++ lesson05/hw/mrmey17/mrmey17.py
@@ -0,0 +1,27 @@
+"У програмі генерується випадкове ціле число від 0 до 100. Користувач повинен його відгадати не більше ніж за 10 спроб."
+"Після кожної невдалої спроби повинно повідомлятися чи більше чи менше введене користувачем число, ніж те, що загадане."
+"Якщо за 10 спроб число не відгадане, то вивести загадане число."
+
+from random import randint
+
+def guess_the_number():
+    rand_num = randint(0, 100)
+    #print(rand_num)
+    try_count = 10
+    for _ in range(10):
+        try_to_guess = int(input('Try to guess the number: '))
+        if try_to_guess == rand_num:
+            print(f'You guessed the number!, "{rand_num}" was a hidden number')
+            break
+        elif try_to_guess < rand_num:
+            print('The hidden number is greater than what you wrote')
+        elif try_to_guess > rand_num:
+            print('The hidden number is less than what you wrote')
+        try_count -= 1
+        print(f'Tries left {try_count}')
+        while not try_count:
+            print('Nice try but the given number was', rand_num)
+            break
+
+guess_the_number()
+
