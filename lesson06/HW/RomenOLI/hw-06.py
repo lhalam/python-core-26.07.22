d380396967774291338031d92b5bef5dc8bfabd7
Roman Oleynik <romenoli@gmail.com>
2022-08-13 12:43:04 +0300

    2 task

--- lesson06/HW/RomenOLI/hw-06.py
+++ lesson06/HW/RomenOLI/hw-06.py
@@ -1,19 +1,32 @@
 #1. Заповнити один список випадковими числами, інший - введеними з клавіатури числами,
 # в третій записати суми відповідних елементів перших двох. Вивести вміст списків на екран.
-from random import randint
+# from random import randint
+#
+# ls_one = list(range(randint(1,30)))
+#
+# n = int(input("please, input the length of second list - "))
+# ls_two, ls_three = [],[]
+# while n>0:
+#     ls_two.append(int(input("please, input numer - ")))
+#     n -=1
+#
+# for i in range(min(len(ls_one), len(ls_two))):
+#     ls_three.append(ls_one[i] + ls_two[i])
+# ls_three = ls_three + (ls_one[len(ls_three):] if len(ls_one)>len(ls_two) else ls_two[len(ls_three):])
+#
+# print("first list -  ", ls_one)
+# print("second list - ", ls_two)
+# print("third list -  ", ls_three)
 
-ls_one = list(range(randint(1,30)))
+#2. Заповнити список дійсними числами введенням з клавіатури. Порахувати суму і добуток елементів списку.
+# Вивести на екран сам список, отримані суму і добуток його елементів.
 
 n = int(input("please, input the length of second list - "))
-ls_two, ls_three = [],[]
+ls, dob = [],1
 while n>0:
-    ls_two.append(int(input("please, input numer - ")))
-    n -=1
-
-for i in range(min(len(ls_one), len(ls_two))):
-    ls_three.append(ls_one[i] + ls_two[i])
-ls_three = ls_three + (ls_one[len(ls_three):] if len(ls_one)>len(ls_two) else ls_two[len(ls_three):])
-
-print("first list -  ", ls_one)
-print("second list - ", ls_two)
-print("third list -  ", ls_three)
+    numb = int(input("please, input numer - "))
+    ls.append(numb)
+    dob *= numb
+    n -= 1
+print(f"Summa = {sum(ls)}")
+print(f"Dobutok = {dob}")
