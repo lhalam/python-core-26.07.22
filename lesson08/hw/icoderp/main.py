f4fb54a0b8e328f15e98bd130ce80e990ab4208a
icoderp <igor.voyt98@gmail.com>
2022-08-23 18:56:33 +0300

    Done hw-08

--- lesson08/hw/icoderp/main.py
+++ lesson08/hw/icoderp/main.py
@@ -2,26 +2,26 @@
 # людини з Вашим віком. При заданих об’єктах p1, p2 і p3, які будуть ініціалізовані name та age,
 # буде повертатися повідомлення наступного формату: {other_person} is {older than / younger than / the same age as} me.
 
-# class Person:
-#     __my_name = "Ivan"
-#     __my_age = 14
-#
-#     def compare_age(self, name, age):
-#         if self.__my_age > age:
-#             print(f"{name} is younger than me.")
-#         elif self.__my_age < age:
-#             print(f"{name} is older than me.")
-#         elif self.__my_age == age:
-#             print(f"{name} is the same age as me.")
-#
-#
-# p1 = Person()
-# p2 = Person()
-# p3 = Person()
-#
-# p1.compare_age('Tom', 44)
-# p2.compare_age('Mick', 13)
-# p3.compare_age('Jon', 14)
+class Person:
+    __my_name = "Ivan"
+    __my_age = 14
+
+    def compare_age(self, name, age):
+        if self.__my_age > age:
+            print(f"{name} is younger than me.")
+        elif self.__my_age < age:
+            print(f"{name} is older than me.")
+        elif self.__my_age == age:
+            print(f"{name} is the same age as me.")
+
+
+p1 = Person()
+p2 = Person()
+p3 = Person()
+
+p1.compare_age('Tom', 44)
+p2.compare_age('Mick', 13)
+p3.compare_age('Jon', 14)
 
 # 2. Визначте атрибути fullname та email в класі Employee. При заданих first та last names:
 # В конструкторі сформуйте fullname звичайним з’єднанням через пробіл first та last name.
@@ -46,3 +46,20 @@
 # атрибут fullname що повертає first і last names;
 # атрибут initials який повертає ініціали (перші літери first та last name, розділених ‘.’ .
 
+# class Name:
+#     fname = ''
+#     lname = ''
+#     fullname = ''
+#     initials = ''
+#
+#     def __init__(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.fname = first_name
+#         self.lname = last_name
+#         self.fullname = f'{first_name} {last_name}'
+#         self.initials = f'{first_name[0]}.{last_name[0]}'
+#
+#
+# n = Name('Jon', 'Sina')
+# print(f"{n.fname=}, {n.lname=}, {n.fullname=}, {n.initials=}")
