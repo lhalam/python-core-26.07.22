aa1727db42b3a1c244bba1dc5649e3bf02f1ca67
Lipfinch <y.svyrydenko@gmail.com>
2022-08-25 17:34:56 +0300

    Lipfinch.py

--- lesson08/hw/Lipfinch HW 8/Lipfinch_HW_ls8.py
+++ lesson08/hw/Lipfinch HW 8/Lipfinch_HW_ls8.py
@@ -2,22 +2,38 @@
 # При заданих об’єктах p1, p2 і p3, які будуть ініціалізовані name та age, буде повертатися повідомлення наступного формату:
 # {other_person} is {older than / younger than / the same age as} me.
 
-class Person:
-    def __init__(self, first_name = '', age = 0):
-        self.name = first_name
-        self.age = age
-    def compare_age(self, other_person):
-        if self.age < other_person.age:
-            return f'other person older than me'
-        elif self.age > other_person.age:
-            return f'other person is younger than me'
-        else:
-            return f'we same age'
-me = Person('Eugene', 32)
-p1 = Person('Clara', 31)
-p2 = Person('Adam,', 26)
-p3 = Person('Alex', 35)
-print(me.compare_age(p1)), print(me.compare_age(p2)), print(me.compare_age(p3))
+# class Person:
+#     def __init__(self, first_name = '', age = 0):
+#         self.name = first_name
+#         self.age = age
+#     def compare_age(self, other_person):
+#         if self.age < other_person.age:
+#             return f'other person older than me'
+#         elif self.age > other_person.age:
+#             return f'other person is younger than me'
+#         else:
+#             return f'we same age'
+# me = Person('Eugene', 32)
+# p1 = Person('Clara', 31)
+# p2 = Person('Adam,', 26)
+# p3 = Person('Alex', 35)
+# print(me.compare_age(p1)), print(me.compare_age(p2)), print(me.compare_age(p3))
+
+# 2.   Визначте атрибути fullname та email в класі Employee. При заданих first та last names:
+#    - В конструкторі сформуйте fullname звичайним з’єднанням через пробіл first та last name.
+#    - В конструкторі сформуйте email з’єднанням first та last name через ‘.’ між ними та приєднуючи  ‘@company.com’ наприкінці.
+
+class Empoyee:
+    def __init__(self, first_name='', last_name=''):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = first_name + last_name
+        self.email = first_name + '.' + last_name + '@company.com'
+    def __str__(self):
+        return f'Emplpoyee:  {self.full_name} has email {self.email}'
+
+p1 = Empoyee('Eugene', ' Svyrydenko')
+print(p1)
 
 
 
