daa56587480050cbed8c5b7d458b3ac2793d09e5
Lipfinch <y.svyrydenko@gmail.com>
2022-08-25 17:26:32 +0300

    Lipfinch.py

--- lesson08/hw/Lipfinch HW 8/Lipfinch_HW_ls8.py
+++ lesson08/hw/Lipfinch HW 8/Lipfinch_HW_ls8.py
@@ -3,9 +3,25 @@
 # {other_person} is {older than / younger than / the same age as} me.
 
 class Person:
-    first name = ''
-    last name = ''
-    age = 0
+    def __init__(self, first_name = '', age = 0):
+        self.name = first_name
+        self.age = age
+    def compare_age(self, other_person):
+        if self.age < other_person.age:
+            return f'other person older than me'
+        elif self.age > other_person.age:
+            return f'other person is younger than me'
+        else:
+            return f'we same age'
+me = Person('Eugene', 32)
+p1 = Person('Clara', 31)
+p2 = Person('Adam,', 26)
+p3 = Person('Alex', 35)
+print(me.compare_age(p1)), print(me.compare_age(p2)), print(me.compare_age(p3))
+
+
+
+
 
 
 
