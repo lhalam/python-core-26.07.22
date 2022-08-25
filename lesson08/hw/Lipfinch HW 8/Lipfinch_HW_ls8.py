29eeeaa4953ca020a2f54205890c415fa294e3b1
Lipfinch <y.svyrydenko@gmail.com>
2022-08-25 15:48:00 +0300

    Lipfinch.py

---
+++ lesson08/hw/Lipfinch HW 8/Lipfinch_HW_ls8.py
@@ -0,0 +1,11 @@
+# 1. В класі Person визначте метод `compare_age()`, який повертатиме результат порівняння віку людини з Вашим віком.
+# При заданих об’єктах p1, p2 і p3, які будуть ініціалізовані name та age, буде повертатися повідомлення наступного формату:
+# {other_person} is {older than / younger than / the same age as} me.
+
+class Person:
+    first name = ''
+    last name = ''
+    age = 0
+
+
+
