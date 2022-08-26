a325aee2e145c6c1fc7bd2b0eb7c14ac90f2f47c
Liubomyr Halamaha <galamagal86@gmail.com>
2022-08-26 15:59:52 +0300

    added lesson09

---
+++ lesson09/fibo.py
@@ -0,0 +1,28 @@
+# print("fibo.py", __name__)
+# import sys
+# from pprint import pprint
+# pprint(sys.path)
+#
+# import temp2
+#
+# print(temp2.a)
+#
+# from temp2 import a, b, c, d, f
+# print(f("aaaa"))
+# def f():
+#     print(__name__)
+#
+# a = 55
+# print(a)
+# f()
+# from temp2 import *
+# print(a)
+#
+# import lesson02.les02
+# import lesson08.hw.Lipfinch_HW_8.Lipfinch_HW_ls8 as l
+# l.Person
+# from lesson08.hw.Lipfinch_HW_8.Lipfinch_HW_ls8 import Person
+print(dir())
+from temp2 import *
+from temp2 import c
+print(dir())
