6917383bf1866a8a1482533267ead8b9382851ec
Roman Oleynik <romenoli@gmail.com>
2022-09-04 21:00:38 +0300

    1 task

--- lesson10/hw/RomenOLI/hw-10.py
+++ lesson10/hw/RomenOLI/hw-10.py
@@ -1,1 +1,10 @@
 #1. CheckEmail
+def valid_email(email):
+    Try:
+        if email.count("@")>1:
+            raise ValueError("email error")
+    except ValueError as err:
+        print("Email is not valid")
+
+
+print(valid_email(input("input email - ")))
