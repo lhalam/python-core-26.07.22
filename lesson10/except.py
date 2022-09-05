59cf2732077046954db3f4aa260a034f79f66681
Liubomyr Halamaha <galamagal86@gmail.com>
2022-09-05 19:53:40 +0300

    add lesson 11

--- lesson10/except.py
+++ lesson10/except.py
@@ -71,46 +71,47 @@
 #         print("Exception", type(err), err)
 #     finally:
 #         print("end")
+#
+# import logging
+#
+# logging.basicConfig(filename='app.log',
+#                     filemode='w',
+#                     format='%(process)d-%(asctime)s-  %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# class ZnamenykError(ArithmeticError):
+#     def __init__(self, data):
+#         logging.info(f"create ZnamenykError {data}")
+#         logging.debug(f"DDDcreate ZnamenykError {data}")
+#         self.data = data
+#     def __str__(self):
+#         return repr(self.data)
+#
+#
+# class ZnamenykError2(ZnamenykError):
+#     def __init__(self, data):
+#         self.data = data
+#     def __str__(self):
+#         return repr(self.data)
+#
+# def read_znamenyk():
+#     a = input("b:")
+#     if not a.isnumeric():
+#         raise ZnamenykError("my error")
+#     if a == 0:
+#         raise ZnamenykError2("AAA")
+#     return a
+#
+# while True:
+#     a = input("a:")
+#     if a == "exit":
+#         break
+#     try:
+#         b = read_znamenyk()
+#         print(int(a) / int(b))
+#     except (ZeroDivisionError, ValueError) as err:
+#         logging.error(err)
+#     except Exception as err:
+#         logging.critical(err)
+#
+#
+# logging.warning('This will get logged to a file')
 
-import logging
-
-logging.basicConfig(filename='app.log',
-                    filemode='w',
-                    format='%(process)d-%(asctime)s-  %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-class ZnamenykError(ArithmeticError):
-    def __init__(self, data):
-        logging.info(f"create ZnamenykError {data}")
-        logging.debug(f"DDDcreate ZnamenykError {data}")
-        self.data = data
-    def __str__(self):
-        return repr(self.data)
-
-
-class ZnamenykError2(ZnamenykError):
-    def __init__(self, data):
-        self.data = data
-    def __str__(self):
-        return repr(self.data)
-
-def read_znamenyk():
-    a = input("b:")
-    if not a.isnumeric():
-        raise ZnamenykError("my error")
-    if a == 0:
-        raise ZnamenykError2("AAA")
-    return a
-
-while True:
-    a = input("a:")
-    if a == "exit":
-        break
-    try:
-        b = read_znamenyk()
-        print(int(a) / int(b))
-    except (ZeroDivisionError, ValueError) as err:
-        logging.error(err)
-    except Exception as err:
-        logging.critical(err)
-
-
-logging.warning('This will get logged to a file')
