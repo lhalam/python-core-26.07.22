c63c4db5a2afa4ff32605ebf270628aa015c0bad
Larysa Petrova <mamapanama1984@gmail.com>
2022-09-25 20:16:10 +0300

    myproject

--- lesson13/HW/PetrovaLarisa/myproject/hello.py
+++ lesson13/HW/PetrovaLarisa/myproject/hello.py
@@ -16,6 +16,7 @@ Bootstrap(app)
 # DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1@localhost:5432/todolist'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = 'sfjhvdfbhvbkjsdbjbnjhuhrgbiut'
 db = SQLAlchemy(app)
 
@@ -25,21 +26,26 @@ class Task(db.Model):
     name = db.Column(db.String)
     description = db.Column(db.String)
     deadline = db.Column(db.Date)
+    complete = db.Column(db.Boolean)
 
-    def __init__(self, name, description, deadline):
+    def __init__(self, name, description, deadline, complete):
         self.name = name
-        self.description = description
+        self.description =  description
         self.deadline = deadline
+        self.complete = complete
 
     def __repr__(self):
         return self.name
 
 @app.route('/')
-def hello_world():
-    return render_template("hello.html")
+def list_task():
+    #show all tasks
+    tasks = Task.query.all()
+    return render_template("hello.html", tasks = tasks)
 
 
 @app.route('/task', methods=["POST", "GET"])
+#add new item
 def add_task():
     if request.method == 'GET':
         return render_template("add.html")
@@ -47,21 +53,48 @@ def add_task():
             name = request.form["name"]
             description = request.form["description"]
             deadline = request.form["deadline"]
-            tasks = Task(name, description, deadline)
-            db.session.add(tasks)
+            complete = False
+            task = Task(name, description, deadline, complete)
+            db.session.add(task)
             db.session.commit()
             return redirect(url_for('add_task'))
 
+@app.route('/edit/<int:task_id>', methods = ['GET', 'POST'])
+#edit item
+def edit_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    if request.method == 'POST':
+        task.name = request.form["name"]
+        task.description = request.form["description"]
+        task.deadline = request.form["deadline"]
+        task.complete = False
+        db.session.commit()
+        return redirect(url_for('list_task'))
+    else:
+        return render_template("post_edit.html", task = task)
 
-@app.route('/tasks', methods = ["GET"])
-def list_task():
-    tasks = db.session.query(Task).all()
-    return render_template("hello.html", tasks=tasks)
 
+@app.route('/update/<int:task_id>')
+#update task
+def update(task_id):
+    task = Task.query.filter_by(id = task_id).first()
+    task.complete = not task.complete
+    db.session.commit()
+    return redirect(url_for('list_task'))
+
+@app.route('/delete/<int:task_id>')
+#delete task
+def delete(task_id):
+    task = Task.query.filter_by(id = task_id).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('list_task'))
 
 with app.test_request_context():
 
     url_for('static', filename='style.css')
 
 if __name__ == '__main__':
+    db.create_all()
+
     app.run()
